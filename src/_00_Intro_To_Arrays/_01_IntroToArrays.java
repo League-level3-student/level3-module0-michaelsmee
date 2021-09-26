4d19319069fd3b7b9a14da7510fb7625fbeac7a6
root <root@8d8aecae30d8>
2021-09-26 19:46:24 +0000

    level 3

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,16 +5,24 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] games = new String[5];
+    	
+    	games[0] = "csgo";
+    	games[1] = "fortnite";
+    	games[2] = "minecraft";
+    	games[3] = "roblox";
+    	games[4] = "among us";
         // 2. print the third element in the array
-
+    	System.out.println(games[2]);
         // 3. set the third element to a different value
-
+    	games[2] = "battlefront";
         // 4. print the third element again
-
+    	System.out.println(games[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for(int i =0; i<5; i++) {
+    		games[i] = "";
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
 
