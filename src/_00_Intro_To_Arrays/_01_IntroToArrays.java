6e3d4d3d0aca1a685a10e792fe9b46ea77154f3a
root <root@b1ce5792cae0>
2021-10-03 19:51:16 +0000

    finished arrays

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -21,24 +21,47 @@ public class _01_IntroToArrays {
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
     	for(int i =0; i<5; i++) {
-    		games[i] = "";
+    		games[i] = "hi";
     	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+    	for(int i=0; i<games.length; i++) {
+    		System.out.println(games[i]);
+    	}
         // 7. make an array of 50 integers
-
+    	int[] numbers = new int[50];
+    	
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	Random rand = new Random();
+    	for(int i=0; i<numbers.length; i++) {
+    		numbers[i]=rand.nextInt(50);
+    	}
+    	for(int i=0; i<numbers.length; i++) {
+    		System.out.println(numbers[i]);
+    	}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+    	int x = numbers[0];
+    	int y = numbers[0];
+    	for(int i=0; i<numbers.length; i++) {
+    		
+    		if(numbers[i]<x) {
+    			x=numbers[i];
+    		}
+    	}
+    	for(int i=0; i<numbers.length; i++) {
+    		if(numbers[i]>y) {
+    			y=numbers[i];
+    		}
+    	}
+    	System.out.println("Smallest number is " + x);
+    	System.out.println("Largest number is " + y);
         // 10 print the entire array to see if step 8 was correct
-
+    	
         // 11. print the largest number in the array.
-
+    	
         // 12. print only the last element in the array
-
+    	System.out.println(numbers[49]);
     }
 }
